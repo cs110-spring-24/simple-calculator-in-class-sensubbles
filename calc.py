a151dc2b863e8f74599a75d3d0e0bdca4cb4425a
rooneybewildin <rooneyrojas2004@gmail.com>
2024-02-05 16:15:59 -0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,25 @@
+num1 = int(input("write down your first number: "))
+op = input("write down your operation symbol: ")
+num2 = int(input("write down your second number: "))
 
+if op == "+":
+	total = num1 + num2
+	print(f"{num1} + {num2} is {total}")
+if op == "-":
+	total = num1 - num2
+	print(f"{num1} - {num2} is {total}")
+if op == "*":
+	total = num1 * num2
+	print(f"{num1} * {num2} is {total}")
+if op == "/":
+	total = num1 // num2
+	print(f"{num1} / {num2} is {total}")
+if op == "^":
+	total = num1 ** num2
+	print(f"{num1} to the power of {num2} is {total}")
+if op == "//":
+	total = num1 // num2
+	print(f"the nearest integer of {num1} / {num2} is {total}")
+if op == "%":
+	total = num1 % num2
+	print(f"The remainder of: {num1} / {num2} is {total}")
